c8aa4e6177d49d078a27337c96c9d76e84a81207
Vladimir Somov <Vladimir.v.somov@gmail.com>
2024-06-20 15:04:24 +0300

    Исправить Жене

--- Volodya_assistant.py
+++ Volodya_assistant.py
@@ -1,5 +1,5 @@
 from sound_functions import listen, say
-import volodya_functions as vf
+import volodya_functions.volodya_functions as vf
 
 
 # Список команд
@@ -13,11 +13,13 @@ names = ['володя', 'вован', 'вовчик', 'вова']
 
 # Бесконечный цикл, ожидающий команд
 while True:
+    backgropn = vf.is_dota2_running()
     text = listen()
     print(text)
     if any(name in text for name in names):
         for command, function in commands.items():
             if command in text:
                 say('Выполняю')
-                function()
+                print(backgropn)
+                function(backgropn)
 
--- volodya_functions/volodya_functions.py
+++ volodya_functions/volodya_functions.py
@@ -1,34 +1,46 @@
 import os
-import webbrowser
 from sound_functions import say
 import sys
 import keyboard
+import psutil
+import subprocess
 
 # Функции джарвиса
 
 
-def open_discord():
+def open_discord(backgropn):
     os.startfile(
         r'C:\Users\sidze\AppData\Local\Discord\app-1.0.9149\Discord.exe')
     say('Диск запущен')
 
 
-def open_youtube():
-    webbrowser.open_new('https://www.youtube.com/')
+def open_youtube(backgropn):
+    chrome_path = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    # Открываем приложение в фоновом режиме
+    subprocess.Popen([chrome_path, 'https://www.youtube.com/'],
+                     stdout=[None, subprocess.DEVNULL][backgropn])
     say('Youtube открыт')
 
 
-def open_tg():
-    os.startfile(
-        r'C:\Users\sidze\AppData\Roaming\Telegram Desktop\Telegram.exe')
-    say('Телеграм запущен')
+def open_tg(backgropn):
+    if not (backgropn):
+        os.startfile(
+            r'C:\Users\sidze\AppData\Roaming\Telegram Desktop\Telegram.exe')
 
 
-def terminate_program():
+def terminate_program(backgropn):
     say('До свидания')
     sys.exit()
 
 
-def game_accept():
+def game_accept(backgropn):
     keyboard.press_and_release('enter')
     say('Приятной игры!')
+
+
+def is_dota2_running():
+    # Проверяем все запущенные процессы
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'] and "dota2" in process.info['name'].lower():
+            return True
+    return False
