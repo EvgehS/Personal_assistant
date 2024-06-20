13010745d8a54c694f0e0072ffbc641cfe06058e
Vladimir Somov <Vladimir.v.somov@gmail.com>
2024-06-20 15:04:24 +0300

    Исправить Жене

--- volodya_functions/background_functions.py
+++ volodya_functions/background_functions.py
@@ -1,9 +1,3 @@
-import psutil
 
 
-def is_dota2_running():
-    # Проверяем все запущенные процессы
-    for process in psutil.process_iter(['pid', 'name']):
-        if process.info['name'] and "dota2" in process.info['name'].lower():
-            return True
-    return False
+
