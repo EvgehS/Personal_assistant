62ac6be1f3a4f98bdb81d032de0b7999abeae81f
Vladimir Somov <Vladimir.v.somov@gmail.com>
2024-06-27 15:34:51 +0300

    для себя

--- assistant_functions/assistant_functions.py
+++ assistant_functions/assistant_functions.py
@@ -21,26 +21,31 @@ def is_dota2_running():
 def open_discord():
     subprocess.Popen([paths['discord']],
                      stdout=[None, subprocess.DEVNULL][is_dota2_running()])
-    say('Дис запущен')
+    link = 'C:\\Users\\1\\OneDrive\\Рабочий стол\\sounds\\Дис_запущен.mp3'
+    say(link)
 
 
 def open_youtube():
     subprocess.Popen([paths['chrome'], 'https://www.youtube.com/'],
                      stdout=[None, subprocess.DEVNULL][is_dota2_running()])
-    say('Youtube открыт')
+    link = 'C:\\Users\\1\\OneDrive\\Рабочий стол\\sounds\\Ютуб_открыт.mp3'
+    say(link)
 
 
 def open_tg():
     subprocess.Popen([paths['telegram']],
                      stdout=[None, subprocess.DEVNULL][is_dota2_running()])
-    say('Telegram открыт')
+    link = 'C:\\Users\\1\\OneDrive\\Рабочий стол\\sounds\\Телеграм_открыт.mp3'
+    say(link)
 
 
 def terminate_program():
-    say('До свидания')
+    link = 'C:\\Users\\1\\OneDrive\\Рабочий стол\\sounds\\До_свидания.mp3'
+    say(link)
     sys.exit()
 
 
 def game_accept():
     keyboard.press_and_release('enter')
-    say('Приятной игры!')
+    link = 'C:\\Users\\1\\OneDrive\\Рабочий стол\\sounds\\Приятной_игры.mp3'
+    say(link)
