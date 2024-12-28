bf91813f1a147f084775ec59d53dd9a6aec04207
Evgen <sidzeka2000@gmail.com>
2024-12-29 01:02:36 +0300

    Начало ассистента на плюсах

--- Personal_assistant.py
+++ Personal_assistant.py
@@ -1,32 +1,15 @@
-from Config.config import names
 from Sounds.sound_functions import listen, say
-import assistant_functions.assistant_functions as af
 import logging
 
 logger = logging.getLogger(__name__)
 
-# Список команд
-commands = {'открой ютуб': af.open_youtube,
-            'открой дис': af.open_discord,
-            'открой телеграмм': af.open_tg,
-            'стоп': af.terminate_program,
-            'прими игру': af.game_accept,
-            'включи музыку': af.open_vk}
+logging.basicConfig(level=logging.INFO)
 
 logger.info('Ассистент запущен')
 
-
+say("Sounds/Выполняю.mp3")
 # Бесконечный цикл, ожидающий команд
 while True:
     text = listen()
     if text:
         logger.info(text)
-    if any(name in text for name in names):
-        for command, function in commands.items():
-            if command in text:
-                say('Sounds/Выполняю.mp3')
-                try:
-                    function()
-                except Exception as e:
-                    logger.error(f'Произошла ошибка - {e} '
-                                 'при выполнении функции {function}')
