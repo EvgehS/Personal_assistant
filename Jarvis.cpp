6d31b4bc0c3f1695320b3d0d93f636ea4342865e
Evgen <sidzeka2000@gmail.com>
2025-04-19 14:40:23 +0300

    Jarvis compile with make

--- Jarvis.cpp
+++ Jarvis.cpp
@@ -1,23 +1,22 @@
-#include <boost/python.hpp>
 #include <iostream>
+#include "Sounds/sound_functions.hpp"
 
-int main()
-{
-    Py_Initialize();
-
-    boost::python::object sys = boost::python::import("sys");
-    sys.attr("path").attr("insert")(1, "Sounds/");
-
-    boost::python::object recognize_speech = boost::python::import("sound_functions").attr("listen");
-    while(true)
+int main() {
+    std::string command;
+    while (true)
     {
-        std::string command = boost::python::extract<std::string>(recognize_speech());
-
+        command = listen();
+        if (command == "error") {
+            std::cerr << "Ошибка распознавания" << std::endl;
+            continue;
+        }
+        
         std::cout << "Распознанная команда: " << command << std::endl;
+        if (command == "стоп") {
+            std::cout << "Выход из программы." << std::endl;
+            break;
+        }
     }
-
     Py_Finalize();
     return 0;
 }
-
-// compile with: g++ Jarvis.cpp -o Jarvis -I/usr/include/python3.13 -L/usr/lib -lboost_python3 -lpython3.13
